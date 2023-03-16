46d367ec57f0af7be11e262c9b7bdc8276f5dbfb
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-16 07:29:53 +0000

    Detect key press and add level

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
 var gamePattern = [];
+// to keep track whether game has started or not
+var started = false;
+var level = 0;
+
+// Detect key press and call the next sequence on the first keypress
+$(document).keypress(function(){
+   if (!started) {
+      $("#level-title").text('Level ' + level);
+      nextSequence();
+      started = true;
+   }
+});
 
 // select button class (since all buttons have the same class)
 // get the id of the button clicked(the colour) and add to userClickedPattern
@@ -14,6 +26,11 @@ $(".btn").click(function(){
 // function creates a new pattern by generating random numbers between 0-3\
 // used to get a random  colour from the colors array
 function nextSequence(){
+   // increase the level anytime this function is called
+   level ++;
+
+   $("#level-title").text('Level ' + level);
+
    var randomNumber  = Math.floor(Math.random() * 4);
    var randomChosenColour = buttonColours[randomNumber];
    gamePattern.push(randomChosenColour);
@@ -37,3 +54,4 @@ function animatePress(currentColour) {
    }, 100);
 
 }
+
